0911c5d2bde64cec3b7b98dcf747dbf78ff8693f
Valentin Dumas <valentin.dumas@viacesi.fr>
2019-02-18 23:22:05 +0100

    wip: GLFW + GLAD

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,40 @@
 #include "hello.h"
 #include <iostream>
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 /// Main program function
-int main(int argc, char *argv[])
+int main(int argc, char *argv)
 {
+    int width, height;
+    GLFWwindow* window;
+
+    /* Init GLFW */
+    if( !glfwInit() )
+        exit( EXIT_FAILURE );
+
+    window = glfwCreateWindow( 400, 400, "Boing (classic Amiga demo)", NULL, NULL );
+    if (!window)
+    {
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+
+    glfwSetWindowAspectRatio(window, 1, 1);
+
+    glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+    glfwSwapInterval( 1 );
+
+    glfwGetFramebufferSize(window, &width, &height);
+
+    if(!gladLoadGL()) {
+        printf("Something went wrong!\n");
+        exit(-1);
+    }
+    printf("OpenGL %d.%d\n", GLVersion.major, GLVersion.minor);
+
     std::cout << "Setup status: SUCCESS" << std::endl;
     // std::cin.get();
 
