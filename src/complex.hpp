d8b0eff42e11ed55798940d543e3fa93538087ab
Valentin Dumas <valentin.dumas@viacesi.fr>
2019-02-21 02:54:46 +0100

    try doxygen on a class (2)

--- src/complex.hpp
+++ src/complex.hpp
@@ -10,62 +10,62 @@
  * @brief Class that represent complex number.
  */
 
- /*! @class Complex
+ /** @class Complex
   *  @brief A class to represent a Complex number.
   *  A more detailed class description
   */
 class Complex {
 private:
-    /*! @var double real
+    /** @var double real
      *  @brief The real part of the complex number.
      */
     double real;
 
-    /*! @var double imaginary
+    /** @var double imaginary
      *  @brief The imaginary part of the complex number.
      */
     double imaginary;
 
 public:
-    /*! @fn Complex::Complex(double=0, double=0)
+    /** @fn Complex::Complex(double=0, double=0)
      *  @brief Constructor
      */
     Complex(double=0, double=0);
 
-    /*! @fn Complex::Complex(const Complex& rhs)
+    /** @fn Complex::Complex(const Complex& rhs)
      *  @brief Copy constructor
      *  @param rhs the object to copy
      */
     Complex(const Complex& rhs);
 
-    /*! @fn Complex::~Complex()
+    /** @fn Complex::~Complex()
      *  @brief Destructor
      */
     ~Complex();
 
-    /*! @fn Complex& Complex::operator=(const Complex& rhs)
+    /** @fn Complex& Complex::operator=(const Complex& rhs)
      *  @param rhs
      *  @return the resultant operator after the assigment
      */
-    Complex& operator=(const Complex& rhs); //!< Assigment operator
+    Complex& operator=(const Complex& rhs);
 
-    /*! @fn Complex::getReal()
+    /** @fn Complex::getReal()
     *   @brief Get the real part
     *   @return The complex's real part
     */
     double getReal() const;
 
-    /*! @fn Complex::getImaginary()
+    /** @fn Complex::getImaginary()
     *   @brief Get the imaginary part
     *   @return The complex's imaginary part
     */
     double getImag() const;
 
-    /*! @fn Complex::abs()
+    /** @fn Complex::abs()
     *   @brief Get the absolute (sqrt(R² + I²))
     *   @return The complex's absolute
     */
     double abs() const;
 };
 
-#endif //CI_OPENGL_TEMPLATE_COMPLEX_HPP
+#endif
