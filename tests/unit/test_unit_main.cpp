c9d11d7475ec947ab03e1a58efff36eb7d355663
Valentin Dumas <valentin.dumas@viacesi.fr>
2019-02-22 16:35:19 +0100

    try Separated Integration && Unit tests (with Codecov) (8)

--- tests/unit/test_unit_main.cpp
+++ tests/unit/test_unit_main.cpp
@@ -4,8 +4,8 @@
 
 #include <iostream>
 
-#include "gtest/gtest.h"
-#include "gmock/gmock.h"
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 using ::testing::AtLeast;
 
 TEST(TestSuiteEquality, EQual)
@@ -20,9 +20,6 @@ TEST(TestSuiteEquality, NonEQual)
 
 int test_unit_main(int argc, char **argv)
 {
-    ::testing::InitGoogleTest(&argc, argv);
-    ::testing::InitGoogleMock(&argc, argv);
-
     int unit_test_status = RUN_ALL_TESTS();
 
     return unit_test_status;
