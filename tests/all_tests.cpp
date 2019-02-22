debad8f781b87849fc64f242a285cb8a2283b683
Valentin Dumas <valentin.dumas@viacesi.fr>
2019-02-22 22:03:08 +0100

    Cleanup CMakeLists.txt && config files

---
+++ tests/all_tests.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Spark on 22/02/2019.
+//
+
+#include <iostream>
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+using ::testing::AtLeast;
+
+int all_tests_main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    ::testing::InitGoogleMock(&argc, argv);
+
+    int unit_test_status = RUN_ALL_TESTS();
+
+    return unit_test_status;
+}
