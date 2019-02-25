f254da2d85d089030c8a0ad65aef0ffb1713ba48
Valentin Dumas <valentin.dumas@viacesi.fr>
2019-02-25 13:51:10 +0100

    Codacy file for doxygen clean doc (without deps)

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 
 #include <iostream>
 
-#include "glad/glad.h"
-#include "GLFW/glfw3.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 /// Main program function
 int main(int argc, char **argv)
