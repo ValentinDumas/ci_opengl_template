7975e15e5007007a8d310fbc91be04e70093dcd9
Valentin Dumas <valentin.dumas@viacesi.fr>
2019-02-28 12:45:39 +0100

    glm && stb with cmake

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,43 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+// Include all GLM core / GLSL features
+#include <glm/glm.hpp> // vec2, vec3, mat4, radians
+// Include all GLM extensions
+#include <glm/ext.hpp> // perspective, translate, rotate
+
+glm::mat4 transform(glm::vec2 const& Orientation, glm::vec3 const& Translate, glm::vec3 const& Up)
+{
+	glm::mat4 Proj = glm::perspective(glm::radians(45.f), 1.33f, 0.1f, 10.f);
+	glm::mat4 ViewTranslate = glm::translate(glm::mat4(1.f), Translate);
+	glm::mat4 ViewRotateX = glm::rotate(ViewTranslate, Orientation.y, Up);
+	glm::mat4 View = glm::rotate(ViewRotateX, Orientation.x, Up);
+	glm::mat4 Model = glm::mat4(1.0f);
+	return Proj * View * Model;
+}
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+
 /// Main program function
 int main(int argc, char **argv)
 {
-//    int result = add(1, 2);
-//    std::cout << "Result = " << result << std::endl;
+	glm::mat4 test_glm(1.f);
+	test_glm = transform(glm::vec2(1.f, 1.f), glm::vec3(1.f, 1.f, 1.f), glm::vec3(1.f, 1.f, 1.f));
+
+	//    int result = add(1, 2);
+	//    std::cout << "Result = " << result << std::endl;
+
+	int w;
+	int h;
+	int comp;
+	std::string filename_abs = "C:\\Users\\Spark\\Desktop\\apps\\cppprojects\\ci_opengl_template\\src\\jenkins-logo.png";
+	unsigned char* image = stbi_load(filename_abs.c_str(), &w, &h, &comp, STBI_rgb);
+	if (image == nullptr)
+		throw(std::string("Failed to load texture"));
+	else
+		std::cout << "Image " << filename_abs.c_str() << " loaded successfully !" << std::endl;
 
     int width, height;
     GLFWwindow* window;
