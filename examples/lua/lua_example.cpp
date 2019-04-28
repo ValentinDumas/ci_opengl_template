c542928bad343bdd8478c35f914517b809d0b485
Valentin Dumas <valentin.dumas@viacesi.fr>
2019-04-28 17:51:41 +0200

    LuaScript class (+ examples) && Can Manage scripts as assets

--- examples/lua/lua_example.cpp
+++ examples/lua/lua_example.cpp
@@ -1,23 +1,36 @@
-//
-// Created by Spark on 28/04/2019.
-//
+#include <iostream>
+#include <string>
 
-extern "C" {
-#include "lua.h"
-#include "lauxlib.h"
-#include "lualib.h"
+#include "LuaScript.h"
+
+int main() {
+    LuaScript script("assets\\scripts\\Player.lua");
+    float posX = script.get<float>("player.position.x");
+    float posY = script.get<float>("player.position.y");
+    std::string filename = script.get<std::string>("player.filename");
+    int hp = script.get<int>("player.HP");
+
+    std::cout<<"Position X = "<<posX<<", Y = "<<posY<<std::endl;
+    std::cout<<"Filename:"<<filename<<std::endl;
+    std::cout<<"HP:"<<hp<<std::endl;
+
+    // getting arrays
+    std::vector<int> v = script.getIntVector("array");
+    std::cout<<"Contents of array:";
+    for(std::vector<int>::iterator it = v.begin();
+            it != v.end();
+            it++) {
+            std::cout<<*it<<",";
+    }
+    std::cout<<std::endl;
+
+    // getting table keys:
+    std::vector<std::string> keys = script.getTableKeys("player");
+    std::cout<<"Keys of [player] table:";
+    for(std::vector<std::string>::iterator it = keys.begin();
+            it != keys.end();
+            it++) {
+            std::cout<<*it<<",";
+    }
+    std::cout<<std::endl;
 }
-
-class LuaScript
-{
-public:
-
-};
-
-int main(int argc, char **argv)
-{
-
-
-    return 0;
-}
-
--- examples/opengl3_box2D/opengl3_box2D_example.cpp
+++ examples/opengl3_box2D/opengl3_box2D_example.cpp
@@ -101,14 +101,14 @@ int main(int argc, char** argv)
     // Decide GL+GLSL versions
 #if __APPLE__
     // GL 3.2 + GLSL 150
-    const char* glsl_version = "#version 150";
+    //const char* glsl_version = "#version 150";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
 #else
     // GL 3.0 + GLSL 130
-    const char* glsl_version = "#version 130";
+    //const char* glsl_version = "#version 130";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
