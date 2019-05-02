7a075a87fb89d6f1c1f2de9d6aa9897bc8b238b7
Valentin Dumas <valentin.dumas@viacesi.fr>
2019-05-02 10:00:06 +0200

    MSVC_PLATFORM as a CMake definition (if MSVC is used)

---
+++ assets/scripts/tada.lua
@@ -0,0 +1,8 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Spark
+-- Date: 02/05/2019
+-- Time: 09:53
+-- To change this template use File | Settings | File Templates.
+--
+
--- tools/UtilFunctions.cmake
+++ tools/UtilFunctions.cmake
@@ -99,9 +99,9 @@ function(add_custom_executable target_name sources includes_scope includes_strin
 
     target_link_libraries(${target_name} PRIVATE ${libraries_string})
 
-    if (MSVC)
-        target_compile_definitions(${target_name} PRIVATE MSVC_PLATFORM)
-    endif()
+#    if (MSVC)
+#        target_compile_definitions(${target_name} PRIVATE MSVC_PLATFORM)
+#    endif()
 
     MoveNeededResourcesToTarget(${target_name})
 endfunction()
